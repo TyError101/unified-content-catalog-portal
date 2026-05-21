b417a52a2d99871f1e0fd960f34efd07c3f0c8c3
RISHI JOSHI <rishi08092005@gmail.com>
2026-05-21 06:52:07 +0000

    Build add item dialog with save functionality

--- webapp/controller/Catalog.controller.js
+++ webapp/controller/Catalog.controller.js
@@ -1,6 +1,10 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Fragment"
+], function (
+    Controller,
+    Fragment
+) {
     "use strict";
 
     return Controller.extend(
@@ -16,6 +20,72 @@ sap.ui.define([
 
                     "catalog"
                 );
+            },
+
+            onOpenDialog: async function () {
+
+                if (!this.oDialog) {
+
+                    this.oDialog =
+                        await Fragment.load({
+
+                            name:
+                            "com.rishi.contentcatalogportal.view.fragments.AddItem",
+
+                            controller: this
+                        });
+
+                    this.getView()
+                        .addDependent(this.oDialog);
+                }
+
+                this.oDialog.open();
+            },
+
+            onCloseDialog: function () {
+
+                this.oDialog.close();
+            },
+
+            onSaveItem: function () {
+
+                var oModel =
+                    this.getView()
+                        .getModel("catalog");
+
+                var aItems =
+                    oModel.getProperty("/items");
+
+                var sId =
+                    sap.ui.getCore()
+                        .byId("dialogInputId")
+                        .getValue();
+
+                var sName =
+                    sap.ui.getCore()
+                        .byId("dialogInputName")
+                        .getValue();
+
+                var sCategory =
+                    sap.ui.getCore()
+                        .byId("dialogSelectCategory")
+                        .getSelectedKey();
+
+                var oNewItem = {
+
+                    id: sId,
+                    name: sName,
+                    category: sCategory
+                };
+
+                aItems.push(oNewItem);
+
+                oModel.setProperty(
+                    "/items",
+                    aItems
+                );
+
+                this.oDialog.close();
             }
         }
     );
