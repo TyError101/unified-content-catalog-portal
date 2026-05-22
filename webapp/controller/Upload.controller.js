cf9050ec0965e75ac8f9bfd28105927ea2bf71f2
RISHI JOSHI <rishi08092005@gmail.com>
2026-05-22 03:47:33 +0000

    Build stable upload module with upload history

--- webapp/controller/Upload.controller.js
+++ webapp/controller/Upload.controller.js
@@ -1,10 +1,86 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast"
+], function (
+    Controller,
+    JSONModel,
+    MessageToast
+) {
     "use strict";
 
     return Controller.extend(
         "com.rishi.contentcatalogportal.controller.Upload",
-        {}
+        {
+
+            onInit: function () {
+
+                var oData = {
+
+                    uploads: []
+                };
+
+                var oModel =
+                    new JSONModel(oData);
+
+                this.getView().setModel(
+                    oModel,
+                    "upload"
+                );
+            },
+
+            onProcessUpload: function () {
+
+                var oInput =
+                    document.getElementById(
+                        "realFileInput"
+                    );
+
+                var oFile =
+                    oInput.files[0];
+
+                if (!oFile) {
+
+                    MessageToast.show(
+                        "Please select a file"
+                    );
+
+                    return;
+                }
+
+                var oModel =
+                    this.getView()
+                        .getModel("upload");
+
+                var aUploads =
+                    oModel.getProperty("/uploads");
+
+                var oUploadData = {
+
+                    fileName: oFile.name,
+
+                    fileType: oFile.type,
+
+                    fileSize:
+                        (oFile.size / 1024)
+                        .toFixed(2) + " KB",
+
+                    uploadDate:
+                        new Date()
+                        .toLocaleString()
+                };
+
+                aUploads.push(oUploadData);
+
+                oModel.setProperty(
+                    "/uploads",
+                    aUploads
+                );
+
+                MessageToast.show(
+                    "File Uploaded Successfully"
+                );
+            }
+        }
     );
 });
